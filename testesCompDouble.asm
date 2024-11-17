c297e390c865dddbef3ec9ce674c74f56283f3cd
juliaduboisas <jujubadubois@gmail.com>
2024-11-17 11:05:33 -0300

    Added testing files for comparisons and branches with doubles

---
+++ testesCompDouble.asm
@@ -0,0 +1,37 @@
+.data
+	param: .double 1.0
+	v0: .double 0.5
+	v1: .double 1.0
+	v2: .double 1.1
+	lessthan: .asciiz "The number is less than or equal to 1.0\n"
+	gtthan: .asciiz "The number is greater than 1.0\n"
+	noneWork: .asciiz "This did not work"
+.text
+
+.globl main
+main:
+	l.d $f0, param
+	l.d $f2, v2
+	c.le.d $f2, $f0
+	bc1f 0, printLTE
+	bc1t 0, printGT
+	jal none
+printLTE:
+	la $a0, lessthan
+	li $v0, 4
+	syscall
+	jal endpg
+printGT:
+	la $a0, gtthan
+	li $v0, 4
+	syscall
+	jal endpg
+none:
+	la $a0, noneWork
+	li $v0, 4
+	syscall
+	jal endpg
+endpg:
+	li $v0, 10
+	syscall
+	
